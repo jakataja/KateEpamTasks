4dfb411d70c6ed909e9d622a8a61507be73bac9d
Katarzyna Bak <Katarzyna_Bak@epam.com>
2016-04-20 17:06:01 +0200

    fixing\improvement

--- Task1_KFox/app/js/kfox.js
+++ Task1_KFox/app/js/kfox.js
@@ -1,84 +1,96 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', function () {
     
-    "use strict";
+    'use strict';
     
-    document.getElementById("hamburger").addEventListener("click", function () {
-        var menu = document.getElementsByTagName("body")[0];        
-        menu.className = (menu.className === "") ? "menu-on" : "" ;
-    });
-    
-    document.getElementsByTagName("nav")[0].addEventListener("click", function (e) {
-        e = e || event;
-        e.preventDefault();
-        var menu = document.getElementsByTagName("body")[0];        
-        menu.className = (menu.className === "") ? "menu-on" : "" ;
-        window.setTimeout(function(){
-            window.location = e.target;
-        }, 500);
-    });
-    
-            
-    var song = document.getElementById("kfox-audio"),
-        mobile = (window.innerWidth < 768) ? true : false,        
-        playBtn = (mobile) ? document.getElementById("np-play") : document.getElementById("kfox-play"),        
-        stopBtn = (mobile) ? document.getElementById("np-stop") : document.getElementById("kfox-stop"),             
-        volBtn = (mobile) ? document.getElementById("volume") : document.getElementById("kfox-volume"),
-        nextBtn = document.getElementById("kfox-next"),
-        slider = document.getElementById("kfox-slider"),
+	var hamburger = document.querySelector('#hamburger'),
+		page = document.querySelector('body'),
+		nav = document.querySelector('nav'),
+		song = document.querySelector('#kfox-audio'),
+        mobile = (window.innerWidth < 768) ? true : false,
+        playBtn = (mobile) ? document.querySelectorAll('.play')[1] : document.querySelectorAll('.play')[0],
+        stopBtn = (mobile) ? document.querySelectorAll('.stop')[1] : document.querySelectorAll('.stop')[0],
+        volBtn = (mobile) ? document.querySelectorAll('.vol')[0] : document.querySelectorAll('.vol')[1],
+        nextBtn = document.querySelector('#kfox-next'),
+        slider = document.querySelector('#kfox-slider'),
         currtime;
-            
-    function play() {                
-        if (song.currentTime === 0){            
-            song.play();        
-            slider.setAttribute("max", song.duration);
-        } else if (song.paused) {    
+
+    function play() {
+        if (song.currentTime === 0) {
             song.play();
-        } else song.pause();
-        
-        if(!song.paused) this.src = "images/np_controlls_pause.png";
-        else this.src = "images/np_controlls_play.png";        
-    };
+            slider.max = song.duration;
+        } else if (song.paused) {
+            song.play();
+        } else {
+			song.pause();
+		}
+		
+        if (!song.paused) {
+			this.src = 'images/np_controlls_pause.png';
+		} else {
+			this.src = 'images/np_controlls_play.png';
+		}
+    }
     
     function stop() {
         song.pause();
         song.currentTime = 0;
-        playBtn.src = "images/np_controlls_play.png";        
-    };
+        playBtn.src = 'images/np_controlls_play.png';
+    }
     
     function mute() {
         song.muted = !song.muted;
-        if(song.muted) {            
-            if(mobile) this.src = "images/mute.png";
-            else this.src = "images/kfox_mute.png";                
+        if (song.muted) {
+            if (mobile) {
+				this.src = 'images/mute.png';
+			} else {
+				this.src = 'images/kfox_mute.png';
+			}
         } else {
-            if(mobile) this.src = "images/volume.png"; 
-            else this.src = "images/kfox_volume.png";        
-        }        
+            if (mobile) {
+				this.src = 'images/volume.png';
+			} else {
+				this.src = 'images/kfox_volume.png';
+			}
+        }
+    }
+	
+	hamburger.addEventListener('click', function () {
+        page.className = (page.className === '') ? 'menu-on' : '';
+    });
+    
+    nav.addEventListener('click', function (e) {
+        e = e || event;
+        e.preventDefault();        
+		page.classList.toggle('menu-on');
+        window.setTimeout(function () {
+            window.location = e.target;
+        }, 500);
+    });
+	
+    playBtn.addEventListener('click', play);
+    stopBtn.addEventListener('click', stop);
+    volBtn.addEventListener('click', mute);
+    
+    song.ontimeupdate = function () {
+        if (!mobile) {
+			slider.value = parseInt(this.currentTime, 10);
+		}
     };
     
-    playBtn.addEventListener("click", play);
-    stopBtn.addEventListener("click", stop);     
-    volBtn.addEventListener("click", mute);
-    
-    song.ontimeupdate = function() {                
-        if(!mobile) slider.value = parseInt(this.currentTime, 10);
-    };
-    
-    
-    
-    window.addEventListener("resize", function(){
+    window.addEventListener('resize', function () {
                 
         var res = mobile;
-        mobile = (window.innerWidth < 768) ? true : false;  
+        mobile = (window.innerWidth < 768) ? true : false;
         
-        if(mobile !== res){            
-            playBtn = (mobile) ? document.getElementById("np-play") : document.getElementById("kfox-play");        stopBtn = (mobile) ? document.getElementById("np-stop") : document.getElementById("kfox-stop");
-            volBtn = (mobile) ? document.getElementById("volume") : document.getElementById("kfox-volume");
-            playBtn.addEventListener("click", play);
-            stopBtn.addEventListener("click", stop);     
-            volBtn.addEventListener("click", mute);
+        if (mobile !== res) {
+			playBtn = (mobile) ? document.querySelectorAll('.play')[1] : document.querySelectorAll('.play')[0];
+			stopBtn = (mobile) ? document.querySelectorAll('.stop')[1] : document.querySelectorAll('.stop')[0];
+			volBtn = (mobile) ? document.querySelectorAll('.vol')[0] : document.querySelectorAll('.vol')[1];
+            playBtn.addEventListener('click', play);
+            stopBtn.addEventListener('click', stop);
+            volBtn.addEventListener('click', mute);
         }
     });
-};
+});
 
 
