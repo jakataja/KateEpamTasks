6aa952dbdc18e3d3ff27ab34a7f6dfa3d1cd9a87
Katarzyna Bak <Katarzyna_Bak@epam.com>
2016-04-26 17:15:14 +0200

    fixed for ie9 (pollyfills), replaced img for icons

--- Task1_KFox/app/js/kfox.js
+++ Task1_KFox/app/js/kfox.js
@@ -1,116 +1,119 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-	'use strict';
+  'use strict';
 
-	var hamburger = document.querySelector('#hamburger'),
-		page = document.querySelector('body'),
-		nav = document.querySelector('nav'),
-		song = document.querySelector('#kfox-audio'),
-		mobile = (window.innerWidth < 768) ? true : false,
-		playBtn = document.querySelectorAll('.play'),
-		stopBtn = document.querySelectorAll('.stop'),
-		volBtn = document.querySelector('#kfox-volume'),
-		skipBtn = document.querySelectorAll('.next'),
-		slider = document.querySelector('#kfox-slider');
+  var hamburger = document.querySelector('#hamburger'),
+    page = document.querySelector('body'),
+    nav = document.querySelector('nav'),
+    song = document.querySelector('#kfox-audio'),
+    mobile = (window.innerWidth < 768) ? true : false,
+    playBtn = document.querySelectorAll('.icon-play'),
+    stopBtn = document.querySelectorAll('.icon-stop'),
+    skipBtn = document.querySelectorAll('.icon-forward'),
+    volBtn = document.querySelector('.icon-volume-up'),
+    slider = document.querySelector('#kfox-slider');
 
-	function play() {
-		if (song.currentTime === 0) {
-			song.play();
-			slider.max = song.duration;
-		} else if (song.paused) {
-			song.play();
-		} else {
-			song.pause();
-		}
+  function play() {
 
-		if (!song.paused) {
-			playBtn[0].src = 'images/np_controlls_pause.png';
-			playBtn[1].src = 'images/np_controlls_pause.png';
-		} else {
-			playBtn[0].src = 'images/np_controlls_play.png';
-			playBtn[1].src = 'images/np_controlls_play.png';
-		}
-	}
+    if (song.paused) {
+      song.play();
+    } else {
+      song.pause();
+    }
 
-	function stop() {
-		song.pause();
-		song.currentTime = 0;
-		playBtn[0].src = 'images/np_controlls_play.png';
-		playBtn[1].src = 'images/np_controlls_play.png';
-	}
+    playBtn[0].classList.toggle('icon-play');
+    playBtn[0].classList.toggle('icon-pause');
+    playBtn[1].classList.toggle('icon-play');
+    playBtn[1].classList.toggle('icon-pause');
+  }
 
-	function mute() {
-		song.muted = !song.muted;
-		volBtn.classList.toggle('icon-volume-up');
-		volBtn.classList.toggle('icon-volume-off');
-	}
+  function stop() {
+    song.pause();
+    song.currentTime = 0;
 
-	function moveTo() {
-		song.currentTime = slider.value;
-	}
+    playBtn[0].classList.add('icon-play');
+    playBtn[1].classList.add('icon-play');
 
-	function skip() {
-		song.currentTime += 10;
-		slider.value = song.currentTime;
-	}
+    playBtn[0].classList.remove('icon-pause');
+    playBtn[1].classList.remove('icon-pause');
+  }
 
-	hamburger.addEventListener('click', function () {
-		page.className = (page.className === '') ? 'menu-on' : '';
-	});
+  function mute() {
+    song.muted = !song.muted;
+    volBtn.classList.toggle('icon-volume-up');
+    volBtn.classList.toggle('icon-volume-off');
+  }
 
-	volBtn.addEventListener('click', mute);
+  function moveTo() {
+    song.currentTime = slider.value;
+  }
 
-	nav.addEventListener('click', function (e) {
-		e = e || event;
-		e.preventDefault();
-		page.classList.toggle('menu-on');
-		window.setTimeout(function () {
-			window.location = e.target;
-		}, 500);
-	});
+  function skip() {
+    song.currentTime += 10;
+    slider.value = song.currentTime;
+  }
 
-	if (mobile) {
-		playBtn[1].addEventListener('click', play);
-		stopBtn[1].addEventListener('click', stop);
-		skipBtn[1].addEventListener('click', skip);
-	} else {
-		playBtn[0].addEventListener('click', play);
-		stopBtn[0].addEventListener('click', stop);
-		skipBtn[0].addEventListener('click', skip);
-		slider.addEventListener('change', moveTo);
-	}
+  hamburger.addEventListener('click', function () {
+    page.className = (page.className === '') ? 'menu-on' : '';
+  });
 
-	song.ontimeupdate = function () {
-		if (!mobile) {
-			slider.value = parseInt(this.currentTime, 10);
-		}
-	};
+  volBtn.addEventListener('click', mute);
 
-	window.addEventListener('resize', function () {
+  nav.addEventListener('click', function (e) {
+    e = e || event;
+    e.preventDefault();
+    page.classList.toggle('menu-on');
+    window.setTimeout(function () {
+      window.location = e.target;
+    }, 500);
+  });
 
-		var res = mobile;
-		mobile = (window.innerWidth < 768) ? true : false;
+  if (mobile) {
+    playBtn[1].addEventListener('click', play);
+    stopBtn[1].addEventListener('click', stop);
+    skipBtn[1].addEventListener('click', skip);
+  } else {
+    playBtn[0].addEventListener('click', play);
+    stopBtn[0].addEventListener('click', stop);
+    skipBtn[0].addEventListener('click', skip);
+    slider.addEventListener('change', moveTo);
+  }
 
-		if (mobile !== res) {
+  song.onloadedmetadata = function () {
+    slider.max = this.duration;
+  };
 
-			if (mobile) {
-				playBtn[1].addEventListener('click', play);
-				stopBtn[1].addEventListener('click', stop);
-				skipBtn[1].addEventListener('click', moveTo);
-				playBtn[0].removeEventListener('click', play);
-				stopBtn[0].removeEventListener('click', stop);
-				skipBtn[0].removeEventListener('click', skip);
-				slider.removeEventListener('change', moveTo);
+  song.ontimeupdate = function () {
+    if (!mobile) {
+      slider.value = parseInt(this.currentTime, 10);
+    }
+  };
 
-			} else {
-				playBtn[0].addEventListener('click', play);
-				stopBtn[0].addEventListener('click', stop);
-				skipBtn[0].addEventListener('click', moveTo);
-				playBtn[1].removeEventListener('click', play);
-				stopBtn[1].removeEventListener('click', stop);
-				skipBtn[1].removeEventListener('click', skip);
-				slider.addEventListener('change', moveTo);
-			}
-		}
-	});
+  window.addEventListener('resize', function () {
+
+    var res = mobile;
+    mobile = (window.innerWidth < 768) ? true : false;
+
+    if (mobile !== res) {
+
+      if (mobile) {
+        playBtn[1].addEventListener('click', play);
+        stopBtn[1].addEventListener('click', stop);
+        skipBtn[1].addEventListener('click', moveTo);
+        playBtn[0].removeEventListener('click', play);
+        stopBtn[0].removeEventListener('click', stop);
+        skipBtn[0].removeEventListener('click', skip);
+        slider.removeEventListener('change', moveTo);
+
+      } else {
+        playBtn[0].addEventListener('click', play);
+        stopBtn[0].addEventListener('click', stop);
+        skipBtn[0].addEventListener('click', moveTo);
+        playBtn[1].removeEventListener('click', play);
+        stopBtn[1].removeEventListener('click', stop);
+        skipBtn[1].removeEventListener('click', skip);
+        slider.addEventListener('change', moveTo);
+      }
+    }
+  });
 });
